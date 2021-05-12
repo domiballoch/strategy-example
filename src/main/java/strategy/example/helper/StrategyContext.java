d8272aa1f5ca0de3ca90cc8eb57a410dc86a3d7a
Dominic Balloch <dominicballoch@dominics-air.home>
2021-05-12 10:12:08 +0100

    made context class private access

--- src/main/java/strategy/example/helper/StrategyContext.java
+++ src/main/java/strategy/example/helper/StrategyContext.java
@@ -1,5 +1,7 @@
 package strategy.example.helper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import strategy.example.standarddiscounter.Discounter;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StrategyContext {
 
     private Discounter discounter;
